53750fcca3963834a41e3721fcc71f03d118a539
Christos Zosimidis <christos.zosimidis@gmail.com>
2022-10-09 01:06:35 +0200

    Imlement simple sleep functions

---
+++ src/arch/sleep.h
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Christos Zosimidis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * sleep.h
+ *
+ *  Created on: 09.10.2022
+ *      Author: Christos Zosimidis
+ */
+
+#ifndef SRC_ARCH_SLEEP_H_
+#define SRC_ARCH_SLEEP_H_
+
+/* Nordic Semiconductor include. */
+#include <nrfx.h>
+
+/* Standard libraries includes. */
+#include <stdint.h>
+
+__STATIC_INLINE void sleepus(uint32_t microseconds) {
+	NRFX_DELAY_US(microseconds);
+}
+
+__STATIC_INLINE void sleepms(uint32_t miliseconds) {
+	if(miliseconds == 0) {
+		return;
+	}
+
+	do {
+		NRFX_DELAY_US(1000);
+	} while(--miliseconds);
+}
+
+#endif /* SRC_ARCH_SLEEP_H_ */
